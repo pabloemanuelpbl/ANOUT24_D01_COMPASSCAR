abe10dc4e4a70e4b4a45da3d66f2ec82e25d0e40
pabloemanuelpbl <pablo.fju.pe@gmail.com>
2024-11-07 11:53:32 -0300

    docs: removing unnecessary jsdoc typing

--- knex.js
+++ knex.js
@@ -2,7 +2,6 @@ require("dotenv").config();
 const knex = require("knex");
 const knexfile = require("./knexfile.js");
 
-/** @type {import('knex').Knex}*/
 const session = knex(knexfile[process.env.KNEX_ENVIROMENT]);
 
 module.exports = { session };
