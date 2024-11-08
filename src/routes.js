7c7ae0980a42c3b7c50db10cd79aa074aeb317c7
pabloemanuelpbl <pablo.fju.pe@gmail.com>
2024-11-08 09:32:12 -0300

    feat(carItemsUpdate): add usecase to routes

--- src/routes.js
+++ src/routes.js
@@ -1,8 +1,10 @@
 const { Router } = require("express");
 const { carRegistrationContoller } = require("./useCases/carRegistration");
+const { carItemsUpdateController } = require("./useCases/carItemsUpdate");
 
 const router = Router();
 
 router.post("/api/v1/cars", carRegistrationContoller.handle);
+router.put("/api/v1/cars/:id/items", carItemsUpdateController.handle);
 
 module.exports = { router };
