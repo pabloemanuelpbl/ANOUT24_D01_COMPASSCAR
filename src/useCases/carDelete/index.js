72dbd94958a70bf625dbf0b0c30397c80bfbafe5
pabloemanuelpbl <pablo.fju.pe@gmail.com>
2024-11-09 14:34:10 -0300

    feat(deleteCar): implementing deleteCar method in controller

--- src/useCases/carDelete/index.js
+++ src/useCases/carDelete/index.js
@@ -1,3 +1,4 @@
+const carDelete = require("./carDelete");
 const { carDeleteController } = require("./carDeleteController");
 
-module.exports = { carDeleteController: carDeleteController(() => {}) };
+module.exports = { carDeleteController: carDeleteController(carDelete) };
