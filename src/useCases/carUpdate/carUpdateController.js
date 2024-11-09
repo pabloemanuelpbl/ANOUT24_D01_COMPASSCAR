de31c44e997e6265053696f0b1d87e071da4566a
pabloemanuelpbl <pablo.fju.pe@gmail.com>
2024-11-09 20:09:54 -0300

    feat(carUpdate): create test and fix controller

--- src/useCases/carUpdate/carUpdateController.js
+++ src/useCases/carUpdate/carUpdateController.js
@@ -1,5 +1,5 @@
+const { repositoryCars } = require("../../repository/cars");
 const carAlreadyRegistered = require("../utils/carPlateAlreadyRegistered");
-const ifTheCarIsRegistered = require("../utils/ifTheCarIsRegistered");
 
 /**
  * @typedef {import('express').request} Request
@@ -20,13 +20,14 @@ function carUpdateController(carUpdateCallback, carUpdateErrorHandle) {
       const errors = carUpdateErrorHandle(request.body);
       if (errors.length > 0) return response.status(400).json({ errors });
 
-      const carIsRegistered = await ifTheCarIsRegistered(id);
+      const carIsRegistered = await repositoryCars.findOne({ id });
+
       if (!carIsRegistered)
         return response.status(404).json({
           errors: ["car not found"],
         });
 
-      if (request.body.plate) {
+      if (request.body.plate && request.body.plate !== carIsRegistered.plate) {
         const plateIsRegistered = await carAlreadyRegistered(
           request.body.plate
         );
