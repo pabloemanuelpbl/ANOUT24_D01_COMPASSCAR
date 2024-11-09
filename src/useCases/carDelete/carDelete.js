79c5396f40f60033d3c69983cec08243b8c3f5e9
pabloemanuelpbl <pablo.fju.pe@gmail.com>
2024-11-09 14:14:45 -0300

    feat(carDelete): finish the function of deleting car by id

--- src/useCases/carDelete/carDelete.js
+++ src/useCases/carDelete/carDelete.js
@@ -1,5 +1,15 @@
-function carDelete() {
-  return;
+const { repositoryCars } = require("../../repository/cars");
+const { repositoryCarsItems } = require("../../repository/cars_items");
+
+/**
+ * @param {number} carId
+ * @returns {Promise<void>}
+ */
+async function carDelete(carId) {
+  return (
+    await repositoryCarsItems.deleteWhere({ car_id: carId }),
+    await repositoryCars.deleteWhere({ id: carId })
+  );
 }
 
 module.exports = carDelete;
--- src/useCases/carDelete/carDeleteController.js
+++ src/useCases/carDelete/carDeleteController.js
@@ -1,17 +1,33 @@
+const ifTheCarIsRegistered = require("../utils/ifTheCarIsRegistered");
+
 /**
  * @typedef {import('express').request} Request
  * @typedef {import('express').response} Response
  *
  * @param {import("./carDelete")} carDeleteCallback
- * @returns {{handle: Response}}
  */
 function carDeleteController(carDeleteCallback) {
   /**
    * @param {Request} request
    * @param {Response} response
    */
-  function handle(request, response) {
-    return response.status(204).json();
+  async function handle(request, response) {
+    const carId = parseInt(request.params.id);
+
+    const isRegistered = await ifTheCarIsRegistered(carId);
+    if (!isRegistered)
+      return response.status(404).json({
+        errors: ["car not found"],
+      });
+
+    try {
+      await carDeleteCallback(carId);
+      return response.status(204).send();
+    } catch {
+      return response.status(500).json({
+        errors: ["an internal server error occurred"],
+      });
+    }
   }
 
   return { handle };
