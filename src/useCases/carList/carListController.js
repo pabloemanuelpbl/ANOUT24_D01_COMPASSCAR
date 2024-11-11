cc049aab732e8bbad7116cec1763183247ed001a
pabloemanuelpbl <pablo.fju.pe@gmail.com>
2024-11-11 15:13:39 -0300

    feat(carList): create the controller for the carList function

--- src/useCases/carList/carListController.js
+++ src/useCases/carList/carListController.js
@@ -1,18 +1,33 @@
+const { repositoryCars } = require("../../repository/cars");
+
 /**
  * @typedef {import('express').request} Request
  * @typedef {import('express').response} Response
  *
  * @param {import("./carList")} carListCallback
+ * @param {import("./queryGenerator")} queryGeneratorCallback
  */
-function carListController(carListCallback) {
+function carListController(carListCallback, queryGeneratorCallback) {
   /**
    * @param {Request} request
    * @param {Response} response
    */
   async function handle(request, response) {
     try {
-      await carListCallback();
-      return response.status(200).send();
+      const [whereQuery, paginationQuery] = queryGeneratorCallback(
+        request.query
+      );
+      const rawQuery = whereQuery + paginationQuery;
+      const list = await carListCallback(rawQuery);
+      const count = await repositoryCars.count(whereQuery);
+
+      const output = {
+        count: count,
+        pages: Math.ceil(count / (request.query.limit || 5)),
+        data: list,
+      };
+
+      return response.status(200).json(output);
     } catch {
       return response.status(500).json({
         errors: ["an internal server error occurred"],
