98664a18c6365c71cb1f6cb88b0fa017ade60aa0
pabloemanuelpbl <pablo.fju.pe@gmail.com>
2024-11-11 15:12:33 -0300

    feat(carList): create function to list cars

--- src/useCases/carList/carList.js
+++ src/useCases/carList/carList.js
@@ -1,5 +1,15 @@
-function carList() {
-  return;
+const { repositoryCars } = require("../../repository/cars");
+
+/** @typedef {{year: number, final_plate: string, brand: string, page: number, limit: number}} Select */
+
+/**
+ * @param {import("knex").Knex.Raw} rawQuery
+ * @param {Select} [select={}]
+ */
+async function carList(rawQuery, select = {}) {
+  const result = await repositoryCars.find(select).whereRaw(rawQuery);
+
+  return result;
 }
 
 module.exports = carList;
